878bb8249103139a4a3ee6e4e31d7124d709ed4f
Egor <egorzin2003@yandex.ru>
2023-09-27 21:47:25 -0700

    first commit

---
+++ Homeworks/Homework1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// System.Console.WriteLine("input a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("input b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+// {
+//     System.Console.WriteLine($"a = {a}, b = {b} -> max = {a} ");
+// }
+// else
+// {
+//     System.Console.WriteLine($"a = {a}, b = {b} -> max = {b} ");
+// }
+
+//....................................................
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// System.Console.WriteLine("input a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("input b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("input c: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// System.Console.WriteLine($"{a}, {b}, {c} -> {max} ");
+
+ 
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// System.Console.WriteLine("input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n % 2 == 0)
+// {
+//     System.Console.WriteLine($"{n} -> Да");
+// }
+// else
+// {
+//     System.Console.WriteLine($"{n} -> Нет");
+// }
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// System.Console.WriteLine("input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"{n} -> ");
+// int m = 2;
+// System.Console.WriteLine($"{m}, ");
+// if (n > 1)
+// {
+//     while (m < n - 2)
+//     {
+//         System.Console.WriteLine($" {m+=2}, ");
+//     }
+// }
---
+++ Seminars/Sem1/Program.cs
@@ -0,0 +1,56 @@
+﻿//System.Console.WriteLine("input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine($"your number -> {num} ");
+
+//1.Напишите программу, которая на вход принимает число и выдает его квадрат(число, умноженное на само себя)
+
+//System.Console.WriteLine("input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine($"Quad of {num} -> {num*num} ");
+
+//2. напишите программу, которая принимает на вход трехзначное число и на выходе показывает **последнюю** цифру этого числа.
+
+// 456 -> 6   456 % 10 = 45,6
+// 782 -> 2
+// 918 -> 8
+
+//System.Console.WriteLine("input three-digit number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if(num >= 100 && num < 1000)
+//{
+//    System.Console.WriteLine($"Last digit of {num} -> {num%10}");
+//}
+//else
+//{
+//    System.Console.WriteLine("Your input not three-digit number");
+//}
+
+//5. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от - N до N
+// ...........................................
+// System.Console.WriteLine("input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int m = n*(-1);
+// System.Console.WriteLine($"{n} -> \"");
+// while (m < n)
+// {
+//     System.Console.Write($"{m++}, ");
+// }
+// System.Console.WriteLine($"{n}\"");
+//...................................................
+
+//1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//Пример: a =25, b = 5 -> да
+// System.Console.WriteLine("input a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("input b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a == b * b)
+// {
+//     System.Console.WriteLine($"a = {a}, b = {b} -> да");
+// }
+// else
+// {
+//     System.Console.WriteLine($"a = {a}, b = {b} -> нет");
+// }
