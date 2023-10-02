78a8afeed9bf85bddebbdc93c561e99a719e1fbe
Egor <egorzin2003@yandex.ru>
2023-10-01 20:20:41 -0700

    Сделал дз 2

---
+++ Homeworks/Homework2/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// void FirstHomeExample(int num)
+//  {
+//      int dec = num / 10;
+//      int ed = dec % 10;
+//      System.Console.WriteLine($"{num} -> {ed}");
+//  }
+// System.Console.WriteLine("input threeDigitNumber: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// FirstHomeExample(num);
+
+//...................................................
+// Задача 13: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+ 
+//  void SecondHomeExample(int num)
+//   {
+//       if (num >= 100)
+//       {
+//         while (num > 1000)
+//         {
+//             num = num / 10;
+//         }
+//         System.Console.WriteLine($"{num} -> {num % 10}");
+//       }
+//       else
+//       {
+//         System.Console.WriteLine($"{num} -> третьей цифры нет");
+//       }
+//   }
+//  System.Console.WriteLine("input threeDigitNumber: ");
+//  int num = Convert.ToInt32(Console.ReadLine());
+//  SecondHomeExample(num);
+
+//.................................................................
+// Задача 15: Напишите программу, которая принимает на вход 
+// цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+//  bool CheckTheDay(int num)
+//  {
+//      if(num > 5)
+//      {
+//          return true;
+//      }
+//      else
+//      {
+//          return false;
+//      }
+//  }
+//  System.Console.WriteLine("input number: ");
+//  int num = Convert.ToInt32(Console.ReadLine());
+//  System.Console.WriteLine($"{num} -> {CheckTheDay(num)}");
---
+++ Seminars/Sem2/Program.cs
@@ -0,0 +1,94 @@
+﻿// Напишите программу, которая будет принимать
+// на вход два числа и выводить, является ли второе
+// число кратным первому. Если число 2 не кратно числу
+// 1, то программа выводит остаток от деление.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+
+// void CheckMultiplicity(int num1, int num2)
+// {
+//     if (num1 % num2 == 0)
+//     {
+//         System.Console.WriteLine($"{num1}, {num2}  -> кратно");
+      
+//     }
+//     else 
+//     {
+//         System.Console.WriteLine($"{num1}, {num2}  -> Не кратно, остаток {num1 % num2}");
+//     }
+// }
+// System.Console.WriteLine("input number 1: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("input number 2: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// CheckMultiplicity(num1, num2);
+
+//............................................................................
+// Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+// int BiggestDigit(int num)
+// {
+//     int ed = num % 10;
+//     int dec = num /10;
+//     if (ed > dec)
+//     {
+//         return ed;
+//     }
+//     else
+//     {
+//         return dec;
+//     }
+    
+// }
+// int num = new Random().Next(10,100);
+// System.Console.WriteLine(num);
+// int result = BiggestDigit(num);
+// System.Console.WriteLine(BiggestDigit(num));
+
+//............................................................................
+// Напишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+
+// void TwoDigitnumber(int num)
+// {
+//     int hund = num / 100;
+//     int ed = num % 10;
+//     System.Console.WriteLine($"{num} -> {hund * 10 + ed}");
+// }
+// int num = new Random().Next(100,1000);
+// TwoDigitnumber(num);
+
+// ..............................................................................
+// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+// bool CheckMultiplicity(int num)
+// {
+//     if(num % 7 == 0 && num % 23 == 0)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// }
+// System.Console.WriteLine("input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"{num} -> {CheckMultiplicity(num)}");
+ 
+ //.............................................................................
+ 
